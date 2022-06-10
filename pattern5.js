e8671ec13e1994f9b3ad16b5edbecb0d35d59fcc
Bishnu Mishra <bishnumishra01996@gmail.com>
2022-06-10 07:39:27 +0530

    quare pattern print

---
+++ pattern5.js
@@ -0,0 +1,13 @@
+let n = 3;
+let i = 1;
+let count = n * n;
+while(i <= n){
+    let j = 1;
+    while(j <= n){
+        process.stdout.write(String(count)+" ");
+        count = count - 1;
+        j = j + 1;
+    }
+    console.log()
+    i = i + 1;
+}
