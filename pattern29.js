097d0fe80b5091d9f9af3e2d505bb36520608b96
Bishnu Mishra <bishnumishra01996@gmail.com>
2022-06-11 11:52:46 +0530

    angle pattern

---
+++ pattern29.js
@@ -0,0 +1,16 @@
+let n =5;
+let i = 1;
+while(i<=n){
+    let space = 1;
+    while(space<i){
+        process.stdout.write(" ");
+        space = space + 1;
+    }
+    let j = 1;
+    while(j<=n-i+1){
+        process.stdout.write(String(i));
+        j = j + 1;
+    }
+    console.log()
+    i = i + 1;
+}
